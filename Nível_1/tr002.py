2909ed6b8c9ac7a164a2ace9359a49e9c3c136b6
Felipe Alencar Lima <felipe.alencarlima@gmail.com>
2025-11-22 21:19:01 -0300

    Update tr002.py

--- Nível_1/tr002.py
+++ Nível_1/tr002.py
@@ -1,3 +1,4 @@
-nome = input('qual o seu nome? ')
-idade = input('qual a sua idade? ')
-print(f'ola {nome}, voce  tem {idade} anos')
+nome = input('qual o seu nome? ') # ler o nome do usuário 
+idade = input('qual a sua idade? ') # ler a idade do usuário 
+print(f'ola {nome}, voce  tem {idade} anos') # montra o nome e a idade do usuário junto com a mensagem 
+
